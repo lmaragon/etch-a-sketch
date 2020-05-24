1d8778f194f83534f1d9922c2f07ebf9a1f838c3
Luis Aragon <lmaragon@gmail.com>
2020-05-24 09:41:49 -0700

    Add color trail when mouse over cell

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const grid_container = document.querySelector('.container');
 
-console.log(grid_container);
-
 // Create grid
 for(let i=0; i<256; i++){
     grid_container.innerHTML += '<div class="cell"></div>';
 }
+
+const cells = document.querySelectorAll('.cell');
+
+// Listen to hover
+cells.forEach(cell => {
+    cell.addEventListener('mouseover', (el) => {
+        cell.style.backgroundColor = "red";
+        
+    });
+});
